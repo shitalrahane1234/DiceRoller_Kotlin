9fedc93f7227f99fdf70d4953275d15115bb8662
shital rahane <shitalrahane1234@gmail.com>
2020-09-19 15:16:24 +0530

    dynamic text on btn

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init();
+    }
+    fun init():Unit{
+        val roll_btn: Button = findViewById(R.id.btn_roll)
+        roll_btn.text = "Let's Roll"
     }
 }
