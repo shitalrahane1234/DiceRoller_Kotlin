25c608534349d9357bf900ada1fc5da9a5aa36a3
shital rahane <shitalrahane1234@gmail.com>
2020-09-27 19:39:47 +0530

    added colorview with constraintlayout

---
+++ app/src/main/java/com/example/colorViews/ColorMyViews.kt
@@ -0,0 +1,11 @@
+package com.example.colorViews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ColorMyViews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color_my_views)
+    }
+}
